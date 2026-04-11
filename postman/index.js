1a4598097b2c54b7daa2443ac5359022bdedb89a
Yug <yugchoudhary947@gmail.com>
2026-04-11 23:53:41 +0530

    P

--- postman/index.js
+++ postman/index.js
@@ -4,7 +4,7 @@ const app = express();
 const bookstore = [
     {id:1,name:"harry potter",author:"devflux"},
     {id:2,name:"frids",author:"vikas"},
-    {id:3,name:"nexxus",author:"rohit"},
+    {id:3,name:"nexus",author:"rohit"},
     {id:4,name:"dsa",author:"yug"},
     {id:5,name:"holling",author:"tkay"}
 ]
